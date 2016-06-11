d79eff7073398a2a27dc1e8ffcbf35d02d07d397
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-11 12:31:42 +0200

    Added Festival seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Artist database
 
 scuba = Arist.create(name: "Scuba", description: "Scuba is a British electronic musician now based in Berlin. His style has been described as dubstep with a later 'shift toward a brighter and more eclectic approach to production'.", genre: "Dubstep", country: "The United Kingdom", photo: "http://togetherboston.com/wp-content/uploads/scubaB.jpeg")
 nina_kraviz = Artist.create(name: "Nina Kraviz", description: "There are hypnotic and sensuous qualities to all the music this Siberian makes: it stems from the way she works, using her voice as an instrument.", genre: "Techno", country: "Russia", photo: "https://news.beatport.com/nl/files/2016/04/ninakraviz_301115.jpg")
@@ -17,5 +14,10 @@ paul_mccartney = Artist.create(name: "Paul McCartney", description: "Sir James P
 muse = Artist.create(name: "Muse", description: "Muse are an English rock band from Teignmouth, Devon, formed in 1994.", genre: "Rock", country: "The United Kingdom", photo: "http://lolp3.de.c3cdn.com/wp/wp-content/uploads/2015/06/Muse.jpg")
 disclosure = Artist.create(name: "Disclosure", description: "Disclosure are an English electronic music duo consisting of brothers Howard (born 11 May 1994) and Guy Lawrence (born 25 May 1991).", genre: "UK Garage", country: "The United Kingdom", photo: "http://fp-murphy-images-production.s3.amazonaws.com/wp-content/uploads/2015/05/disclosure.jpg")
 
-pinkpop = Festival.create(start_date: end_date: name: "Pinkpop", description: "Pinkpop is a three-day festival, from Saturday through Monday, with a 60,000 people per day capacity, and with performances on 4 separate stages. During 44 editions, more than 2 million people have attended Pinkpop. Some 700 music acts have played at the festival.", price: 95, photo: "http://images0.tcdn.nl/filmenuitgaan/article24154717.ece/BINARY/u/PINKPOP_EPA.JPG", location: "Megaland, Landgraaf", genre: "Pop")
-lowlands = Festival.create(start_date: end_date: name: "Lowlands", description: "A Campingflight to Lowlands Paradise, mostly just known as Lowlands, is an annual three-day music and performing arts festival, held in the Netherlands in August since 1993. Although the main focus is on music - rock, pop, dance, hip hop and alternative - Lowlands also offers indoor and outdoor cinema, (street) theatre, cabaret and stand-up, ballet, literature and comic strips.", price: 175.0, photo: "http://tpo.nl/wp-content/uploads/2014/08/20140814_JBV_LOW_004.jpg", location: "Walibi World, Biddinghuizen", genre: "Pop")
+#Festival database
+
+pinkpop = Festival.create(start_date: (Time.now) end_date: (Time.now + 2.days) name: "Pinkpop", description: "Pinkpop is a three-day festival, from Saturday through Monday, with a 60,000 people per day capacity, and with performances on 4 separate stages. During 44 editions, more than 2 million people have attended Pinkpop. Some 700 music acts have played at the festival.", price: 95.0, photo: "http://images0.tcdn.nl/filmenuitgaan/article24154717.ece/BINARY/u/PINKPOP_EPA.JPG", location: "Megaland, Landgraaf", genre: "Pop")
+lowlands = Festival.create(start_date: (Time.now + 5.weeks) end_date: (Time.now + 158.days) name: "Lowlands", description: "A Campingflight to Lowlands Paradise, mostly just known as Lowlands, is an annual three-day music and performing arts festival, held in the Netherlands in August since 1993. Although the main focus is on music - rock, pop, dance, hip hop and alternative - Lowlands also offers indoor and outdoor cinema, (street) theatre, cabaret and stand-up, ballet, literature and comic strips.", price: 175.0, photo: "http://tpo.nl/wp-content/uploads/2014/08/20140814_JBV_LOW_004.jpg", location: "Walibi World, Biddinghuizen", genre: "Pop")
+awakenings = Festival.create(start_date: (Time.now + 2.weeks), end_date: (Time.now + 16.days), name: "Awakenings", description: "the most exhilarating techno festival in the benelux", price: 65.0, photo: "http://www.kbkvisuals.com/wp-content/uploads/2015/07/19375367059_4fa0624fbc_o-1140x761.jpg", location: "Amsterdam", genre: "Techno")
+a_day_at_the_park = Festival.create(start_date: (Time.now + 3.weeks) , end_date: (Time.now + 24.days), name: "A day at the park", description: "a whole different experience of the usual walk in the park", price: 49.0, photo: "http://www.yourlittleblackbook.me/wp-content/uploads/2015/06/ADATP-2015-3.jpg", location: "Amsterdam", genre: "Techhouse")
+into_the_woods = Festival.create(start_date: (Time.now + 4.weeks), end_date: (Time.now + 31.days), name: "into the woods", description: "gnomes of the forrest, the aroma of fresh leaves and music all together on a beautiful location", price: 55.0, photo: "http://www.festivalfever.nl/wp-content/uploads/2013/05/into-the-woods-600x320.jpg", location: "Amersfoort", genre: "House")
